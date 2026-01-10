02f5351e42659b0c6085e2d46af9c04afea5fe51
sibgatullah-dev <sibgatullah881@gmail.com>
2026-01-10 12:30:52 +0600

    edit function

--- ToDos/ToDo/data/urls.py
+++ ToDos/ToDo/data/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('add/', views.add_task, name='add'),
-    
+    path('edit/<int:task_id>/', views.edit_task, name='edit')
 ]
--- ToDos/ToDo/data/views.py
+++ ToDos/ToDo/data/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Data
 from .forms import DataForm
 
@@ -7,7 +7,6 @@ def index(request):
     return render(request, 'data/index.html', {'data':data})
 
 def add_task(request):
-    data = None
     if request.method == "POST":
         form = DataForm(request.POST, request.FILES)
         if form.is_valid():
@@ -19,4 +18,17 @@ def add_task(request):
         form = DataForm()
         
         
+    return render(request, 'data/data_form.html', {'form':form})
+
+def edit_task(request, task_id):
+    data = get_object_or_404(Data, pk=task_id )
+    if request.method == "POST":
+        form = DataForm(request.POST, request.FILES, instance=data)
+        if form.is_valid():
+            data = form.save(commit=False)
+            data.user = request.user
+            data.save()
+            return redirect('index')
+    else:
+        form = DataForm(instance=data)
     return render(request, 'data/data_form.html', {'form':form})
